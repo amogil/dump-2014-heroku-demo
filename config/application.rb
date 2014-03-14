520cbdffacb371be10f100d771bde5664a7f1f69
Alexey Mogilnikov <alexey@mogilnikov.name>
2014-03-14 10:33:18 +0600

    trying to fix pool interval

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+NewRelic::Binding::Config.poll_cycle_period = 5
+
 module Dump2014HerokuDemo
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
